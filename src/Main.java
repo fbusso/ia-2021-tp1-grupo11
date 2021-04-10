97426d3dd06cd6b9685360a8776748236191e696
Francisco Busso <francisco.busso@outlook.com>
2021-04-10 00:08:09 -0300

    Docs

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,8 @@ import frsf.cidisi.faia.simulator.SearchBasedAgentSimulator;
 public class Main {
 
     public static void main(String[] args) {
+        // Obtener el escenario inicial para inicializar el agente y el ambiente.
         char[][] escenario = AuxiliarEscenario.obtenerMatriz();
-
-
-
-
         Agente agente = new Agente(escenario);
         Ambiente ambiente = new Ambiente(escenario);
         SearchBasedAgentSimulator simulador = new SearchBasedAgentSimulator(ambiente, agente);
