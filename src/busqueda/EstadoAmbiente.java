a7b54012ed474bcd419aaafa09bdf4c11218f5f4
Francisco Busso <francisco.busso@outlook.com>
2021-04-18 19:35:38 -0300

    Cambio de nombre de atributo posicion lobo

--- src/busqueda/EstadoAmbiente.java
+++ src/busqueda/EstadoAmbiente.java
@@ -11,7 +11,7 @@ public class EstadoAmbiente extends EnvironmentState {
     private Integer cantidadTotalDulces;
     private Escenario escenario;
     private Posicion posicionCaperucita;
-    private Posicion posicionActualLobo;
+    private Posicion posicionLobo;
 
     public EstadoAmbiente(Escenario escenario) {
         this.escenario = escenario;
@@ -21,7 +21,7 @@ public class EstadoAmbiente extends EnvironmentState {
     @Override
     public void initState() {
         this.posicionCaperucita = escenario.getPosicionActualCaperucita();
-        this.posicionActualLobo = escenario.getPosicionActualLobo();
+        this.posicionLobo = escenario.getPosicionActualLobo();
         this.cantidadDulcesRecolectados = 0;
         this.cantidadActualVidas = 3;
         this.cantidadTotalDulces = escenario.getCantidadDulces();
@@ -67,19 +67,19 @@ public class EstadoAmbiente extends EnvironmentState {
         this.posicionCaperucita = posicionCaperucita;
     }
 
-    public Posicion getPosicionActualLobo() {
-        return posicionActualLobo;
+    public Posicion getPosicionLobo() {
+        return posicionLobo;
     }
 
-    public void setPosicionActualLobo(Posicion posicionActualLobo) {
-        this.posicionActualLobo = posicionActualLobo;
+    public void setPosicionLobo(Posicion posicionLobo) {
+        this.posicionLobo = posicionLobo;
     }
 
     @Override
     public String toString() {
         return "\n" +
                 "- Posición actual de Caperucita: Fila " + posicionCaperucita.i + ", Columna " + posicionCaperucita.j + "\n" +
-                "- Posición actual del Lobo: Fila " + posicionActualLobo.i + ", Columna " + posicionActualLobo.j + "\n" +
+                "- Posición actual del Lobo: Fila " + posicionLobo.i + ", Columna " + posicionLobo.j + "\n" +
                 "- Cantidad de dulces a recolectar restantes: " + (cantidadTotalDulces - cantidadDulcesRecolectados);
     }
 }
