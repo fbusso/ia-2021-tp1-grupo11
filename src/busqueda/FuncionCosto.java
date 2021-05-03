4147b7d7985857548e1826248e3e5d0985481a9d
Francisco Busso <francisco.busso@outlook.com>
2021-05-03 16:20:24 -0300

    typo

--- src/busqueda/FuncionCosto.java
+++ src/busqueda/FuncionCosto.java
@@ -4,6 +4,7 @@ import frsf.cidisi.faia.solver.search.IStepCostFunction;
 import frsf.cidisi.faia.solver.search.NTree;
 
 public class FuncionCosto implements IStepCostFunction {
+
     @Override
     public double calculateCost(NTree nodo) {
         return nodo.getAction().getCost();
